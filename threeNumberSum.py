afe9417c0a8927443f8dc040f38ccef917d63b39
arkmadj <ahmadjinadu@gmail.com>
2022-10-08 14:54:19 +0100

    Added three number sum

---
+++ threeNumberSum.py
@@ -0,0 +1,26 @@
+from operator import le
+
+
+def threeNumberSum(array, targetSum):
+    array.sort()
+    triplets = []
+    for i in range(len(array) - 2):
+        left = i + 1
+        right = len(array) - 1
+        while left < right:
+            currentSum = array[i] + array[left] + array[right]
+            if currentSum == targetSum:
+                triplets.append([array[i], array[left], array[right]])
+                left += 1
+                right -= 1
+            elif currentSum < targetSum:
+                left += 1
+            elif currentSum > targetSum:
+                right -= 1
+    return triplets
+
+
+a = [-8, -6, 1, 2, 3, 5, 6, 12]
+t = 0
+
+print(threeNumberSum(a, t))
