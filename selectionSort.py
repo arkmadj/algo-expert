33b6b314ab79a7c378984dc5502325fcf67e2282
arkmadj <ahmadjinadu@gmail.com>
2022-10-19 06:56:28 +0100

    Added selection sort

---
+++ selectionSort.py
@@ -0,0 +1,13 @@
+def selectionSort(array):
+  currentIdx = 0
+  while currentIdx < len(array) - 1:
+    smallestIdx = currentIdx
+    for i in range(currentIdx + 1, len(array)):
+      if array[smallestIdx] > array[i]:
+        smallestIdx = i
+    array[currentIdx], array[smallestIdx] = array[smallestIdx], array[currentIdx]
+    currentIdx += 1
+  return array
+
+print(selectionSort([8,5,2,9,5,6,3]))
+        
