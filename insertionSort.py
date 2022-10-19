670eb1f02c73c64661241d4e9ee578c837affdaf
arkmadj <ahmadjinadu@gmail.com>
2022-10-19 06:27:42 +0100

    Added insertion sort

---
+++ insertionSort.py
@@ -0,0 +1,10 @@
+def insertionSort(array):
+  for i in range(1, len(array)):
+    j = i
+    while j > 0 and array[j] < array[j-1]:
+      array[j], array[j-1] = array[j-1], array[j]
+      j -= 1
+  return array
+  
+  
+print(insertionSort([8,5,2,9,5,6,3]))
