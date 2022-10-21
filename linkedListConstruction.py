0de8efe0c97c61277e522cc6e376f9dd66b4c7a0
arkmadj <ahmadjinadu@gmail.com>
2022-10-21 06:22:41 +0100

    linked list construction

---
+++ linkedListConstruction.py
@@ -0,0 +1,48 @@
+class DoublyLinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    
+  def setHead(self, node):
+    pass
+  
+  def setTail(self, node):
+    pass
+  
+  def insertBefore(self, node, nodeToInsert):
+    pass
+  
+  def insertAfter(self, node, nodeToInsert):
+    pass
+  
+  def insertAtPosition(self, position, nodeToInsert):
+    pass
+  
+  def removeNodesWithValue(self, value):
+    node = self.head
+    while node is not None:
+      nodeToRemove = node
+      node = node.next
+      if nodeToRemove.value == value:
+        self.remove(nodeToRemove)
+  
+  def remove(self, node):
+    if node == self.head:
+      self.head = self.head.next
+    if node == self.tail:
+      self.tail = self.tail.prev
+    self.removeNodeBindings(node)
+  
+  def containsNodeWithValue(self, value):
+    node = self.head
+    while node is not None and node.value != value:
+      node = node.next
+    return node is not None
+  
+  def removeNodeBindings(self, node):
+    if node.prev is not None:
+      node.prev.next = node.next
+    if node.next is not None:
+      node.next.prev = node.prev
+    node.prev = None
+    node.next = None
