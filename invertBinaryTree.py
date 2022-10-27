eb329c97776acc6fb58b87d013f7a880fd212a72
arkmadj <ahmadjinadu@gmail.com>
2022-10-27 06:50:01 +0100

    Added invert binary tree

---
+++ invertBinaryTree.py
@@ -0,0 +1,23 @@
+def invertBinaryTree(tree):
+  queue = [tree]
+  while len(queue):
+    current = queue.pop(0)
+    if current is None:
+      continue
+    swapLeftAndRight(current)
+    queue.append(current.left)
+    queue.append(current.right)
+
+def swapLeftAndRight(tree):
+  tree.left, tree.right = tree.right, tree.left
+  
+def invertBinaryTreeRecu(tree):
+  if tree is None:
+    return
+  swapLeftAndRight(tree)
+  invertBinaryTreeRecu(tree.left)
+  invertBinaryTreeRecu(tree.right)
+  
+
+if __name__ == "__main__":
+  pass
