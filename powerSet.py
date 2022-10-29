5569eaf6952b819c0e8205ec2e049413e4b96785
arkmadj <ahmadjinadu@gmail.com>
2022-10-29 06:30:19 +0100

    Added powerset recu

--- powerSet.py
+++ powerSet.py
@@ -7,5 +7,19 @@ def powerSet(array):
   return subsets
 
 
+def powerSetRecu(array, idx = None):
+  if idx is None:
+    idx = len(array) - 1
+  elif idx < 0:
+    return [[]]
+  element = array[idx]
+  subsets = powerSetRecu(array, idx - 1)
+  for i in range(len(subsets)):
+    currentSubset = subsets[i]
+    subsets.append(currentSubset + [element])
+  return subsets
+
+
 a = [1,2,3]
 print(powerSet(a))
+print(powerSetRecu(a))
