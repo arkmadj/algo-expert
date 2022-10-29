cbe586fae7a4ba76b5d992e6c3689b4d4f2e423b
arkmadj <ahmadjinadu@gmail.com>
2022-10-29 06:18:59 +0100

    Added powerset

---
+++ powerSet.py
@@ -0,0 +1,11 @@
+def powerSet(array):
+  subsets = [[]]
+  for element in array:
+    for i in range(len(subsets)):
+      currentSubset = subsets[i]
+      subsets.append(currentSubset + [element])
+  return subsets
+
+
+a = [1,2,3]
+print(powerSet(a))
