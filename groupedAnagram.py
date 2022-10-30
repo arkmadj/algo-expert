728f14f149bb5a15ffb464153a5b7d549e2a92ec
arkmadj <ahmadjinadu@gmail.com>
2022-10-30 22:46:18 +0100

    Added grouped anagrams

---
+++ groupedAnagram.py
@@ -0,0 +1,34 @@
+from unittest import result
+
+
+def groupedAnagrams(words):
+  if len(words) == 0:
+    return []
+
+  sortedWords = ["".join(sorted(w)) for w in words]
+  indices = [i for i in range(len(words))]
+  indices.sort(key=lambda x: sortedWords[x])
+
+  result = []
+  currentAnagramGroup = []
+  currentAnagram = sortedWords[indices[0]]
+  for index in indices:
+    word = words[index]
+    sortedWord = sortedWords[index]
+    
+    if sortedWord == currentAnagram:
+      currentAnagramGroup.append(word)
+      continue
+    
+    result.append(currentAnagramGroup)
+    currentAnagramGroup = [word]
+    currentAnagram = sortedWord
+    
+  result.append(currentAnagramGroup)
+
+  return result
+
+
+a = ["yo", "act", "flop", "tac", "cat", "oy", "olfp"]
+
+print(groupedAnagrams(a))
