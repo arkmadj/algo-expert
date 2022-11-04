90c2d7438e3d2717454e7697d5248828c6063b24
arkmadj <ahmadjinadu@gmail.com>
2022-11-04 07:39:37 +0100

    Added swap and peek methods

--- minHeapConstruction.py
+++ minHeapConstruction.py
@@ -1,6 +1,27 @@
 class MinHeap:
   def __init__(self):
+    self.heap = self.buildHeap(array)
+
+  def buildHeap(self, array):
     pass
+  
+  def siftDown(self, currentIdx, endIdx, heap):
+    pass
+  
+  def siftUp(self, currentIdx, heap):
+    pass
+  
+  def peek(self):
+    return self.heap[0]
+  
+  def remove(self):
+    pass
+  
+  def insert(self, value):
+    pass
+  
+  def insert(self, i, j, heap):
+    heap[i], heap[j] = heap[j], heap[i]
 
 if __name__ == "__main__":
   #  a = [30,102,23,17,18,9,44,12,31]
