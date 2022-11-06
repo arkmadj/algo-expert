26b44c16f7e268f84526dc3a7a0d0c94e5bbcc47
arkmadj <ahmadjinadu@gmail.com>
2022-11-06 07:08:44 +0100

    add insert at end and move remove nth node

--- removeNthNodeFromEnd.py
+++ removeNthNodeFromEnd.py
@@ -5,23 +5,37 @@ class Node:
 class LinkedList:
   def __init__(self) -> None:
     self.head = None
-
-def removeNthNodeFromTheEnd(head, n):
-  counter = 1
-  first = head
-  second = head
-  while counter <= n:
-    second = second.next
-    counter += 1
-  if second is None:
-    head.value = head.next.value
-    head.next = head.next.next
-    return
-  while second.next is not None:
-    second = second.next
-    first = first.next
-  first.next = first.next.next
+    
+  def insertAtEnd(self, new_data):
+    new_node = Node(new_data)
+    
+    if self.head is None:
+      self.head = new_node
+      return
+    
+    last = self.head
+    while(last.next):
+      last = last.next
+      
+    last.next = new_node
+      
+  def removeNthNodeFromTheEnd(self, n):
+    counter = 1
+    first = self.head
+    second = self.head
+    while counter <= n:
+      second = second.next
+      counter += 1
+    if second is None:
+      self.head.value = self.head.next.value
+      self.head.next = self.head.next.next
+      return
+    while second.next is not None:
+      second = second.next
+      first = first.next
+    first.next = first.next.next
   
 
 if __name__ == "__main__":
-  pass
+  a = LinkedList()
+  a.head = Node(0)
