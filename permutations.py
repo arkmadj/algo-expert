e0ff477b288e78144240ab191d05999971f9ebaf
arkmadj <ahmadjinadu@gmail.com>
2022-11-06 08:10:29 +0100

    Added second solution

--- permutations.py
+++ permutations.py
@@ -1,4 +1,4 @@
-def getPemutationsOne(array):
+def getPermutationsOne(array):
   permutations = []
   permutationsHelper(array, [], permutations)
   return permutations
@@ -11,9 +11,28 @@ def permutationsHelper(array, currentPermutation, permutations):
       newArray = array[:i] + array[i+1:]
       newPermutation = currentPermutation + [array[i]]
       permutationsHelper(newArray, newPermutation, permutations)
+      
+def getPermutationsTwo(array):
+  permutations = []
+  permutationsHelperTwo(0, array, permutations)
+  return permutations
+
+def permutationsHelperTwo(i, array, permutations):
+  if i == len(array) - 1:
+    permutations.append(array[:])
+  else:
+    for j in range(i, len(array)):
+      swap(array, i , j)
+      permutationsHelperTwo(i + 1, array, permutations)
+      swap(array, i, j)
+
+def swap(array, i, j):
+  array[i], array[j] = array[j], array[i]
 
 if __name__ == "__main__":
   a = [1,2,3]
   b = [1,2,3, 4,5]
-  print(getPemutationsOne(a))
-  print(getPemutationsOne(b))
+  print(getPermutationsOne(a))
+  print(getPermutationsTwo(a))
+  print(getPermutationsOne(b))
+  print(getPermutationsTwo(b))
