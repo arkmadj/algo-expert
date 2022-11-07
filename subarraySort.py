6d64389970d991ea2ad16ac6165c641f15b0df04
arkmadj <ahmadjinadu@gmail.com>
2022-11-07 08:05:34 +0100

    Added subarray sort

--- subarraySort.py
+++ subarraySort.py
@@ -1,5 +1,27 @@
 def subArraySort(array):
-  pass
+  minOutOfOrder =  float("inf")
+  maxOutOfOrder =  float("-inf")
+  for i in range(len(array)):
+    num = array[i]
+    if isOutOfOrder(i, num, array):
+      minOutOfOrder = min(minOutOfOrder, num)
+      maxOutOfOrder = max(maxOutOfOrder, num)
+  if minOutOfOrder == float("inf"):
+    return [-1, -1]
+  subArrayLeftIdx = 0
+  while minOutOfOrder >= array[subArrayLeftIdx]:
+    subArrayLeftIdx += 1
+  subArrayRightIdx = len(array) - 1
+  while maxOutOfOrder <= array[subArrayRightIdx]:
+    subArrayRightIdx -= 1
+  return [subArrayLeftIdx, subArrayRightIdx]
+  
+def isOutOfOrder(i, num, array):
+  if i == 0:
+    return num > array[i+1]
+  if i == len(array) - 1:
+    return num < array[i - 1]
+  return num > array[i + 1] or num < array[i - 1]
 
 a = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
 print(subArraySort(a))
