d0c080e32a2dd27031e8490b2f712c2e8a64dfa5
arkmadj <ahmadjinadu@gmail.com>
2022-11-08 07:46:26 +0100

    Added third solution

--- minRewards.py
+++ minRewards.py
@@ -42,7 +42,18 @@ def expandFromLocalMinIdx(localMinIdx, scores, rewards):
     rewards[rightIdx] = rewards[rightIdx - 1] + 1
     rightIdx += 1
     
+def minRewardsThree(scores):
+  rewards = [1 for _ in scores]
+  for i in range(1, len(scores)):
+    if scores[i] > scores[i - 1]:
+      rewards[i] = rewards[i - 1] + 1
+  for i in reversed(range(len(scores) - 1)):
+    if scores[i] > scores[i + 1]:
+      rewards[i] = max(rewards[i], rewards[i + 1] + 1)
+  return sum(rewards)
+    
 if __name__ == "__main__":
   a = [8,4,2,1,3,6,7,9,5]   
   print(minRewards(a))
   print(minRewardsTwo(a))
+  print(minRewardsThree(a))
