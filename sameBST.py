bc57ab02d4bb91b1b9068eb07cec58bdeca39b14
arkmadj <ahmadjinadu@gmail.com>
2022-11-09 07:25:37 +0100

    Added second solution

--- sameBST.py
+++ sameBST.py
@@ -27,8 +27,43 @@ def getBiggerOrEqual(array):
       biggerOrEqual.append(array[i])
   return biggerOrEqual
 
+def sameBSTTwo(arrayOne, arrayTwo):
+  return areSameBST(arrayOne, arrayTwo, 0, 0, float("-inf"), float("inf"))
+
+def areSameBST(arrayOne, arrayTwo, rootIdxOne, rootIdxTwo, minVal, maxVal):
+  if rootIdxOne == - 1 and rootIdxTwo == -1:
+    return rootIdxOne == rootIdxTwo
+  
+  if arrayOne[rootIdxOne] != arrayTwo[rootIdxTwo]:
+    return False
+  
+  leftRootIdxOne = getIdxOfFirstSmaller(arrayOne, rootIdxOne, minVal)
+  leftRootIdxTwo = getIdxOfFirstSmaller(arrayTwo, rootIdxTwo, minVal)
+  rightRootIdxOne = getIdxOfFirstBiggerOrEqual(arrayOne, rootIdxOne, maxVal)
+  rightRootIdxTwo = getIdxOfFirstBiggerOrEqual(arrayTwo, rootIdxTwo, maxVal)
+
+  currentValue = arrayOne[rootIdxOne]
+  leftAreSame = areSameBST(arrayOne, arrayTwo, leftRootIdxOne, leftRootIdxTwo, minVal, currentValue)
+  rightAreSame = areSameBST(arrayOne, arrayTwo, rightRootIdxOne, rightRootIdxTwo, currentValue, maxVal)
+  
+  return leftAreSame and rightAreSame
+  
+
+def getIdxOfFirstSmaller(array, startingIdx, minVal):
+  for i in range(startingIdx + 1, len(array)):
+    if array[i] < array[startingIdx] and array[i] >= minVal:
+      return i
+  return -1
+
+def getIdxOfFirstBiggerOrEqual(array, startingIdx, maxVal):
+  for i in range(startingIdx + 1, len(array)):
+    if array[i] >= array[startingIdx] and array[i] < maxVal:
+      return i
+  return -1
+
 if __name__ == "__main__":
   a = [10, 15, 8, 12, 94, 81, 5, 2, 11]
   b = [10, 8, 5, 15, 2, 12, 11, 94, 81]
   
   print(sameBST(a, b))
+  print(sameBSTTwo(a, b))
