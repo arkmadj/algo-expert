7077368f7fb7add041f68238e3d91a31c83a3a9c
arkmadj <ahmadjinadu@gmail.com>
2022-11-09 06:58:39 +0100

    Added first solution

--- sameBST.py
+++ sameBST.py
@@ -1,8 +1,34 @@
-def sameBST(array1, array2):
-  pass
+def sameBST(arrayOne, arrayTwo):
+  if len(arrayOne) != len(arrayTwo):
+    return False
+  if len(arrayOne) == 0 and len(arrayTwo) == 0:
+    return True
+  if arrayOne[0] != arrayTwo[0]:
+    return False
+  
+  leftOne = getSmaller(arrayOne)
+  leftTwo = getSmaller(arrayTwo)
+  rightOne = getBiggerOrEqual(arrayOne)
+  rightTwo = getBiggerOrEqual(arrayTwo)
+  
+  return sameBST(leftOne, leftTwo) and sameBST(rightOne, rightTwo)
+
+def getSmaller(array):
+  smaller = []
+  for i in range(1, len(array)):
+    if array[i] < array[0]:
+      smaller.append(array[i])
+  return smaller
+
+def getBiggerOrEqual(array):
+  biggerOrEqual = []
+  for i in range(1, len(array)):
+    if array[i] >= array[0]:
+      biggerOrEqual.append(array[i])
+  return biggerOrEqual
 
 if __name__ == "__main__":
-  a = [10, 15, 8, 12, 94, 82, 5, 2, 11]
+  a = [10, 15, 8, 12, 94, 81, 5, 2, 11]
   b = [10, 8, 5, 15, 2, 12, 11, 94, 81]
   
   print(sameBST(a, b))
